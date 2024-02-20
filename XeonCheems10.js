ccf9011c097903f8f1da2afed6862431099520c7
sensei-ofc <153446153+sensei-ofc@users.noreply.github.com>
2024-02-20 18:59:58 -0500

    Update XeonCheems10.js

--- XeonCheems10.js
+++ XeonCheems10.js
@@ -24,9 +24,7 @@ const jsobfus = require('javascript-obfuscator')
 const {translate} = require('@vitalets/google-translate-api')
 const scp2 = require('./lib/scraper2') 
 const { exec, spawn, execSync } = require("child_process")
-const {
-    performance
-} = require('perf_hooks')
+const { performance } = require('perf_hooks')
 const more = String.fromCharCode(8206)
 const readmore = more.repeat(4001)
 const { TelegraPh, UploadFileUgu, webp2mp4File, floNime } = require('./lib/uploader')
@@ -58,16 +56,7 @@ const { xeontext5 } = require('./src/data/function/XBug/xeontext5')
 const verifieduser = JSON.parse(fs.readFileSync('./src/data/role/user.json'))
 
 global.db.data = JSON.parse(fs.readFileSync('./src/database.json'))
-if (global.db.data) global.db.data = {
-sticker: {},
-database: {}, 
-game: {},
-others: {},
-users: {},
-chats: {},
-settings: {},
-...(global.db.data || {})
-}
+if (global.db.data) global.db.data = { sticker: {}, database: {}, game: {}, others: {}, users: {}, chats: {}, settings: {}, ...(global.db.data || {}) }
 
 let vote = db.data.others.vote = []
 let kuismath = db.data.game.math = []
@@ -95,14 +84,8 @@ var ytimewisher = `Buenos días 🌄`
 var ytimewisher = `Buenos días 🌄`
  } 
 //función
-const reSize = async(buffer, ukur1, ukur2) => {
-   return new Promise(async(resolve, reject) => {
-      let jimp = require('jimp')
-      var baper = await jimp.read(buffer);
-      var ab = await baper.resize(ukur1, ukur2).getBufferAsync(jimp.MIME_JPEG)
-      resolve(ab)
-   })
-}
+const reSize = async(buffer, ukur1, ukur2) => { return new Promise(async(resolve, reject) => { let jimp = require('jimp'); var baper = await jimp.read(buffer); var ab = await baper.resize(ukur1, ukur2).getBufferAsync(jimp.MIME_JPEG); resolve(ab); }) }
+
 //module
 module.exports = SenseiOfc = async (SenseiOfc, m, chatUpdate, store) => {
     try {
@@ -197,10 +180,10 @@ module.exports = SenseiOfc = async (SenseiOfc, m, chatUpdate, store) => {
         }
         //premium
         async function replyprem(teks) {
-    replygc(`Esta función es para usuarios premium, comuníquese con el propietario para convertirse en usuario premium`)
+    	replygc(`Esta función es para usuarios premium, comuníquese con el propietario para convertirse en usuario premium`)
 }
         //script replier
-        async function sendSenseiOfcMessage(chatId, message, options = {}){
+    async function sendSenseiOfcMessage(chatId, message, options = {}){
     let generate = await generateWAMessage(chatId, message, options)
     let type2 = getContentType(generate.message)
     if ('contextInfo' in options) generate.message[type2].contextInfo = options?.contextInfo
@@ -280,36 +263,10 @@ module.exports = SenseiOfc = async (SenseiOfc, m, chatUpdate, store) => {
 		const floc = {key : {participant : '0@s.whatsapp.net', ...(m.chat ? { remoteJid: `status@broadcast` } : {}) },message: {locationMessage: {name: wm,jpegThumbnail: thumb}}}
 		const fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': ownername, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${ownername},;;;\nFN:${ownername}\nitem1.TEL;waid=916909137213:916909137213\nitem1.X-ABLabel:Mobile\nEND:VCARD`, 'jpegThumbnail': thumb, thumbnail: thumb,sendEphemeral: true}}}
 	        const fakestatus = {key: {fromMe: false,participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "status@broadcast" } : {})},message: { "imageMessage": {"url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc","mimetype": "image/jpeg","caption": wm,"fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=","fileLength": "28777","height": 1080,"width": 1079,"mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=","fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=","directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69","mediaKeyTimestamp": "1610993486","jpegThumbnail": fs.readFileSync('./Media/theme/menu.jpg'),"scansSidecar": "1W0XhfaAcDwc7xh1R8lca6Qg/1bB4naFCSngM2LKO2NoP5RI7K+zLw=="}}}
-	        const frpayment = {
-	key: {
-		remoteJid: '0@s.whatsapp.net',
-		fromMe: false,
-		id: `${ownername}`,
-		participant: '0@s.whatsapp.net'
-	},
-	message: {
-		requestPaymentMessage: {
-			currencyCodeIso4217: "USD",
-			amount1000: 999999999,
-			requestFrom: '0@s.whatsapp.net',
-			noteMessage: {
-				extendedTextMessage: {
-					text: `${botname}`
-				}
-			},
-			expiryTimestamp: 999999999,
-			amount: {
-				value: 91929291929,
-				offset: 1000,
-				currencyCode: "PEN"
-			}
-		}
-	}
-}
+	        const frpayment = { key: { remoteJid: '0@s.whatsapp.net', fromMe: false, id: `${ownername}`, participant: '0@s.whatsapp.net' }, message: { requestPaymentMessage: { currencyCodeIso4217: "USD", amount1000: 999999999, requestFrom: '0@s.whatsapp.net', noteMessage: { extendedTextMessage: { text: `${botname}` } }, expiryTimestamp: 999999999, amount: { value: 91929291929, offset: 1000, currencyCode: "PEN" } } } }
             
-            const pickRandom = (arr) => {
-return arr[Math.floor(Math.random() * arr.length)]
-}
+            const pickRandom = (arr) => { return arr[Math.floor(Math.random() * arr.length)]; }
+
         
         //database
         try {
